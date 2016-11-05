33d20b006b817b525678c0201dce534fb45e7697
ha-li <ha-li@gmail.com>
2016-11-04 21:05:48 -0700

    add more graph functionality

--- src/main/java/com/gecko/graph/GraphUtil.java
+++ src/main/java/com/gecko/graph/GraphUtil.java
@@ -1,5 +1,7 @@
 package com.gecko.graph;
 
+import com.gecko.graph.exception.VertexNotFoundException;
+
 /**
  * Created by hlieu on 11/4/16.
  */
@@ -18,5 +20,29 @@ public class GraphUtil {
         }
 
         return transpose;
+    }
+
+    // returns the out degree of vertex v
+    public static int out_degree(int[][] g, int v) {
+        // error check
+        if(v >= g.length) { throw new VertexNotFoundException(); }
+
+
+        int degree = 0;
+        for(int i = 0; i < g[v].length; i++) {
+            if(g[v][i] > 0) { degree++; }
+        }
+        return degree;
+    }
+
+    // returns the in-degree of vertex v
+    public static int in_degree(int[][] g, int v) {
+        if(v >= g.length) { throw new VertexNotFoundException(); }
+
+        int degree = 0;
+        for(int i = 0; i < g[v].length; i++) {
+            if(g[i][v] > 0) { degree++; }
+        }
+        return degree;
     }
 }
---
+++ src/main/java/com/gecko/graph/exception/VertexNotFoundException.java
@@ -0,0 +1,7 @@
+package com.gecko.graph.exception;
+
+/**
+ * Created by hlieu on 11/4/16.
+ */
+public class VertexNotFoundException extends RuntimeException {
+}
